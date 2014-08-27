11c02c72759dd01d25c183ecd7cc04dda3075333
noahreifsnyder <ndr217@lehigh.edu>
2014-08-27 02:44:28 +0000

    WelcomeClass

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////
+//hw01-WelcomeClass
+//
+public class WelcomeClass{
+
+    public static void main(String[] args)  {
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-N--D--R--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("My two hobbies are soccer and drums. I play soccer in");
+    System.out.println("my free time, and I play drums for the Marching 97.");
+    }
+    
+}
