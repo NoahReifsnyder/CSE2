518b2edb04cbae27570d57e52734ef8dac566951
noahreifsnyder <ndr217@lehigh.edu>
2014-10-24 14:56:56 +0000

    First commit

---
+++ lab09/Methods.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Methods{
+    public static int getInt(int input){
+        Scanner scan=new Scanner(System.in);
+        boolean loop=false;
+	    System.out.print("Enter an int:");
+	    do{
+	       if (scan.hasNextInt()){
+	            input=scan.nextInt();
+	            loop=true;
+	       }//ends if
+            else {
+	           scan.next();//clears input
+	           System.out.print("You did not enter an int, pleast try again:");
+	           loop=false;
+	        }
+	    }while(!loop);//loops till an int is entered
+	    return input;//returns the value entered when method is called
+	}//ends getInt method
+	public static int larger(int a,int b){
+        if (a>b){
+    	    return a;
+    	}//ends if 
+    	else{
+            return b;
+    	}//ends else
+	}//ends method larger
+	public static boolean ascending (int a, int b, int c){
+	    if (a<=b && b<=c){
+	        return true;
+	    }//ends if
+	    else {
+	        return false;
+	    }//ends else
+	}//ends method ascending
+    public static void main(String [] arg){
+	    Scanner scan=new Scanner(System.in);
+	    int a,b,c;
+	    int input=0;
+    	System.out.println("Enter three ints");
+    	a=getInt(input);
+    	b=getInt(input);
+    	c=getInt(input);
+    	System.out.println("The larger of "+a+" and "+b+" is "+larger(a,b));
+    	System.out.println("The larger of "+a+", "+b+", and "+c+" is "+larger(a,larger(b,c)));
+    	System.out.println("It is "+ascending(a,b,c)+" that "+a+", "+b+	", and "+c+" are in ascending order");
+        
+    }//ends main method
+}//ends class
