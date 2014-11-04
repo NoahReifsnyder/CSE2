872685295d631f109905911bc6b08008a9f23623
noahreifsnyder <ndr217@lehigh.edu>
2014-11-04 06:00:33 +0000

    first commit, forgot to commit when finished

---
+++ lab07/LoopTheLoop.java
@@ -0,0 +1,46 @@
+//lab07-LoopTheLoop
+//Noah Reifsnyder
+//10/20/14
+//CSE2
+//Practicing using loops
+import java.util.Scanner;
+public class LoopTheLoop{
+public static void main(String[] args){
+    //define class and main method
+    Scanner myScanner=new Scanner (System.in);//define scanner
+    int nStars;
+    System.out.print("Please enter an int between 1 and 15:");
+    while (!myScanner.hasNextInt()){
+        myScanner.next();
+        System.out.println("You did not enter an int, please try again");
+        System.out.print("Please enter an int between 1 and 15:");
+    }//ends while
+    nStars=myScanner.nextInt();
+    System.out.println(nStars);
+    while (nStars>=16 || nStars<=0){
+        System.out.println("Your input was not between 1 and 15, please try again");
+        System.out.print("Please enter an int between 1 and 15:");
+        while (!myScanner.hasNextInt()){
+            myScanner.next();
+            System.out.println("You did not enter an int, please try again");
+            System.out.print("Please enter an int between 1 and 15:");
+        }//ends while
+    }//ends while
+    int count=0;
+    while (count<nStars){
+        count++;
+        System.out.print("*");
+    }//ends while
+    count=0;
+    int secondCount;
+    while (count<nStars+1){
+        secondCount=0;
+        while (secondCount<count){
+            secondCount++;
+            System.out.print("*");
+        }//ends while
+        System.out.println();
+        count++;
+    }//ends while
+    }//ends main method
+}//ends class
